53482db633f72da0573adf737453e2412652bc61
Aaron Gullickson <aarongullickson@gmail.com>
2022-09-24 10:51:54 -0700

    Update default packages

--- analysis/check_packages.R
+++ analysis/check_packages.R
@@ -5,10 +5,8 @@
 #http://www.vikram-baliga.com/blog/2015/7/19/a-hassle-free-way-to-verify-that-r-packages-are-installed-and-loaded
 
 #add new packages to the chain here
-packages = c("here","readr",
-             "ggplot2","ggalt",
-             "dplyr","tibble","stringr","tidyr","broom",
-             "knitr","texreg","kableExtra")
+packages = c("here","readr","haven","tidyverse","ggplot2","lubridate","stringr",
+             "tidyr","broom","knitr","texreg","kableExtra")
 
 package.check <- lapply(packages, FUN = function(x) {
   if (!require(x, character.only = TRUE)) {
